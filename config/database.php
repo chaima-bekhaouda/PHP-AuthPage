6eb7abcd0221b4be3cb78efd67d1f7d3f653f4f5
Chaima BEKHAOUDA <137110936+chaima-bekhaouda@users.noreply.github.com>
2024-03-06 15:52:19 +0100

    feat: Add database configuration class

---
+++ config/database.php
@@ -0,0 +1,24 @@
+<?php
+
+class Database
+{
+
+    private string $servername = "localhost";
+    private string $username = "root";
+    private string $password = "";
+    private string $dbname = "mywebsite";
+
+    public function connect(): ?PDO
+    {
+        try {
+
+            $bdd = new PDO("mysql:host=$this->servername;dbname=$this->dbname;charset=utf8", $this->username, $this->password);
+            $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $bdd;
+        } catch (PDOException $e) {
+
+            error_log("DB connection failed: " . $e->getMessage());
+            return null;
+        }
+    }
+}
